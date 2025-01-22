5fc0b89ccdc392d8fa065e6a78cfc5ba721e076a
Erley Barbosa da Silva <erleybru@gmail.com>
2025-01-22 15:58:00 -0300

    Deploy ready

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -21,15 +22,19 @@ public class GameService {
 	public GameDTO findById(Long id) {
 		// tratar a excessão caso ID não exista
 		Game result = gameRepository.findById(id).get();
-		
 		return new GameDTO(result);
-		
 	}
 	
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		return result.stream().map(GameMinDTO::new).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByGameList(Long listId) {
+		List<GameMinProjection> games = gameRepository.searchByList(listId);
+		return games.stream().map(GameMinDTO::new).toList();
 	}
 
 }
